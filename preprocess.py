1790e71b2ae8a395ccc467bda5aed6001c488af4
Andrey Sugonyaev <sug.andre606@gmail.com>
2020-04-09 17:55:13 +0300

    add argparse

--- preprocess.py
+++ preprocess.py
@@ -1,3 +1,4 @@
+import argparse
 import cv2
 import numpy as np
 import os
@@ -17,7 +18,7 @@ def video_to_frames(video_path, frames_dir):
     cap.release()
 
 
-def dataset_to_frames(root="GRID/videos", out_root="GRID/frames"):
+def dataset_to_frames(root="GRID/videos/", out_root="GRID/frames/"):
     for subject in os.listdir(root):
         os.mkdir(os.path.join(out_root, subject))
 
@@ -30,3 +31,31 @@ def dataset_to_frames(root="GRID/videos", out_root="GRID/frames"):
             video_path = os.path.join(dir_path, file)
             video_to_frames(video_path, out_dir_path)
         print("Done with subject", subject)
+
+
+def arg_parse():
+    parser = argparse.ArgumentParser(description="Preprocess GRID")
+    parser.add_argument(
+        "--root",
+        dest="root",
+        help="Directory where GRID videos are",
+        default="GRID/videos/",
+        type=str,
+    )
+    parser.add_argument(
+        "--out",
+        dest="out_root",
+        help="Directory where frames are to be saved",
+        default="GRID/frames/",
+        type=str,
+    )
+    return parser.parse_args()
+    
+
+if __name__ == "__main__":
+    args = arg_parse()
+    try:
+        os.mkdir(args.out_root)
+    except:
+        pass
+    dataset_to_frames(args.root, args.out_root)
