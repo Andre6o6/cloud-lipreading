10391564396972bfb19444e2589546d676c12a97
Andrey Sugonyaev <sug.andre606@gmail.com>
2020-04-25 22:51:17 +0300

    quick subtitle render code

---
+++ subtitles.py
@@ -0,0 +1,23 @@
+import ffmpeg
+
+def render_subtitles(video_path, subtitles, offsets=[]):
+    """
+    params:
+        video_path: str  - Path to video
+        subtitle: [str]  - List of subtitle strings
+        offsets: [int]   - List of subtitle offsets in frames
+    """
+    #FIXME
+    sub = subtitles[0]
+    name, ext = video_path.split('.')
+    name = name+"_subbed."+ext
+    
+    file = ffmpeg.input(video_path)
+    audio = file.audio
+    video = file.drawtext(
+            text=sub,
+            fontcolor="white",
+            x="(w-tw)/2",
+            y="h-1.5*th",
+        )
+    ffmpeg.output(video, audio, name).run()
