51ef01815899aa2cb5d0897b9c84249234756303
Andrey Sugonyaev <sug.andre606@gmail.com>
2020-04-26 22:25:09 +0300

    fix tabulation

--- train.py
+++ train.py
@@ -15,36 +15,36 @@ def train(run_name, dataset_path, aligns_path):
     start_time = time.time()
     
     lipnet.model.fit_generator(
-		generator = datagen.train_generator,
-		validation_data = datagen.val_generator,
-		epochs = 1,
-		verbose = 1,
-		shuffle = True,
-		max_queue_size = 5,
-		workers = 2,
-		callbacks=callbacks,
-		use_multiprocessing = True
-	)
+        generator = datagen.train_generator,
+        validation_data = datagen.val_generator,
+        epochs = 1,
+        verbose = 1,
+        shuffle = True,
+        max_queue_size = 5,
+        workers = 2,
+        callbacks=callbacks,
+        use_multiprocessing = True
+    )
     elapsed_time = time.time() - start_time
-	print('\nTraining completed in: {}'.format(datetime.timedelta(seconds=elapsed_time)))
+    print('\nTraining completed in: {}'.format(datetime.timedelta(seconds=elapsed_time)))
     
     
 def create_callbacks(run_name, log_root='log/', checkpoint_root='results/'):
     create_dir(log_root)
     
     run_log_dir = os.path.join(log_root, run_name)
-	create_dir(run_log_dir)
+    create_dir(run_log_dir)
     
     tensorboard = TensorBoard(log_dir=run_log_dir)
     csv_log = os.path.join(run_log_dir, 'training.csv')
-	csv_logger = CSVLogger(csv_log, separator=',', append=True)
+    csv_logger = CSVLogger(csv_log, separator=',', append=True)
     
     create_dir(checkpoint_root)
     
     checkpoint_dir = os.path.join(checkpoint_root, run_name)
-	create_dir(checkpoint_dir)
-	checkpoint_template = os.path.join(checkpoint_dir, "lipnet_{epoch:03d}_{val_loss:.2f}.hdf5")
-	checkpoint = ModelCheckpoint(checkpoint_template, monitor='val_loss', save_weights_only=True, mode='auto', period=1, verbose=1)
+    create_dir(checkpoint_dir)
+    checkpoint_template = os.path.join(checkpoint_dir, "lipnet_{epoch:03d}_{val_loss:.2f}.hdf5")
+    checkpoint = ModelCheckpoint(checkpoint_template, monitor='val_loss', save_weights_only=True, mode='auto', period=1, verbose=1)
 
 
 def create_dir(path):
