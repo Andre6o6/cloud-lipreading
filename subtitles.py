b370cdc7df0021f17bf78ed453055bab71bf4ce7
Andrey Sugonyaev <sug.andre606@gmail.com>
2020-05-04 13:45:11 +0300

    argument cleanup

--- subtitles.py
+++ subtitles.py
@@ -21,12 +21,12 @@ def render_line(video_path, subtitles):
     ffmpeg.output(video, audio, name).global_args( '-loglevel', 'error').run()
 
 
-def render_subtitles(video_path, out_path, subs, offsets=75):
+def render_subtitles(video_path, out_path, subs, window=75, color=(255,255,255), box=True):
     """
     params:
         video_path: str  - Path to video
         subtitle: [str]  - List of subtitle strings
-        offsets: [int]   - subtitle offsets in frames
+        window: int   - subtitle window in frames
 
     """
     vidcap = cv2.VideoCapture(video_path)
@@ -39,8 +39,7 @@ def render_subtitles(video_path, out_path, subs, offsets=75):
     out = cv2.VideoWriter(out_path, fourcc, fps, size)
 
     font = cv2.FONT_HERSHEY_COMPLEX_SMALL
-    color = (255,255,255)
-    box_color = (0,0,0)
+    box_color = (0,0,0) if color[0] > 127 else (255,255,255)
     
     offset_from_bottom = 5
     box_offset = 2
@@ -52,13 +51,14 @@ def render_subtitles(video_path, out_path, subs, offsets=75):
         text_w, text_h = cv2.getTextSize(subs[n_line], font, font_scale, 1)[0]
         x = size[0]//2 - text_w//2
         y = size[1] - offset_from_bottom
-        cv2.rectangle(image, (x,y+box_offset), (x+text_w,y-text_h-box_offset), box_color, -1)
+        if box:
+            cv2.rectangle(image, (x,y+box_offset), (x+text_w,y-text_h-box_offset), box_color, -1)
         cv2.putText(image, subs[n_line],(x,y), font, font_scale, color)
         out.write(image)
 
         ret,image = vidcap.read()
         n_frame +=1
-        if n_frame>offsets:
+        if n_frame>window:
             n_line += 1
             n_frame = 0
     out.release()
